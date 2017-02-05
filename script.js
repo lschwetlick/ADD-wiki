7be19301191bf4c97f3a7f05aa99954550c7e325
Christoph Matthies <christoph.matthies@gmail.com>
2017-02-05 11:57:52 +0100

    Introduce functions.

--- script.js
+++ script.js
@@ -7,28 +7,36 @@ $( document ).ready(function() {
 		console.log( "start_article: ", start_article );
 	});
 
-	var wiki_api_base = "https://en.wikipedia.org/w/api.php?"
-	params = {
-		action: "parse",
-		format: "json",
-		page: "Stack Overflow",
-		redirects: 1,
-		prop: "text",
-		section: 0,
-		disablelimitreport: 1,
-		disableeditsection: 1,
-		disabletoc: 1,
-		noimages: 1
+	/* Returns the query URL for the Wikipedia API of a given page */
+	function wikiApiUrl(page) {
+		var wiki_api_base = "https://en.wikipedia.org/w/api.php?"
+		params = {
+			action: "parse",
+			format: "json",
+			page: page,
+			redirects: 1,
+			prop: "text",
+			section: 0,
+			disablelimitreport: 1,
+			disableeditsection: 1,
+			disabletoc: 1,
+			noimages: 1
+		}
+		/* "callback=?" makes jquery pick a custom name for the callback.
+		JQuery then knows to set the datatype to JSONP.
+		The server wraps the requested JSON in a function call with the callback's name.*/
+		return wiki_api_base + $.param(params) + "&callback=?";
 	}
-	/* "callback=?" makes jquery pick a custom name for the callback.
-	JQuery then knows to set the datatype to JSONP.
-	The server wrapts the requested JSON in a function call with the callback's name.*/
-	query = wiki_api_base + $.param(params) + "&callback=?";
 
-	$.getJSON( query )
+	/* Returns a parsed structure queryable with JQuery from an HTML string */
+	function parseToDOM(html_string) {
+		return $($.parseHTML('<div>' + html_string + '</div>'));
+	}
+
+	$.getJSON( wikiApiUrl('Stack Overflow') )
 		.done(function(data) {
 			console.log("GET success");
-			out = $($.parseHTML('<div>' + data.parse.text["*"] + '</div>'));
+			out = parseToDOM(data.parse.text["*"]);
 
 			out.find('p').children('sup').remove();
 			html = '';
@@ -38,7 +46,7 @@ $( document ).ready(function() {
 				console.log('No link at the end of a sentence found.')
 				return
 			}
-			var sentence = $($.parseHTML('<div>' + split[0] + '</a>.</div>'));
+			var sentence = parseToDOM(split[0]);
 			var last_a = sentence.find('a:last');
 
 			var next_entry = last_a.attr('href').split('/wiki/')[1];
